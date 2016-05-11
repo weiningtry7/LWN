1a5d35c0fd19bd5cf7c20be2de342612b00feb99
lanou3g <1131811951@qq.com>
2016-05-11 20:04:30 +0800

    这是我的一个推送，5月11 号 晚上20.03

---
+++ LWN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWN
+//
+//  Created by lanou3g on 16/5/11.
+//  Copyright © 2016年 lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
